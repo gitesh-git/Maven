de3ef4acee5adb3081d6bbb1043d669f3c199e81
gitesh-git <giteshgithub@gmail.com>
2023-04-09 10:28:28 +0530

    maven project

---
+++ flipkart/src/main/java/com/flipkart/App.java
@@ -0,0 +1,14 @@
+package com.flipkart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello Gitesh!" );
+    }
+}
